7f801a2b56f4700531db7d2714cc3635420b02cc
dam09 <dam09@PC09.aula1.com>
2019-05-02 10:58:33 +0200

    asdadskaas

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,24 @@
+
+package holamundo;
+
+import java.util.Scanner;
+
+/* 
+ * Enunciado de la práctica: HolaMundo.java
+ *
+ *
+ * Autor: Baha
+ *
+ * Fecha de creación de la practica: 02-may-2019, 10:54:06
+ * Fecha de finalización de la práctica: Por determinar.
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hola mundo!");
+        System.out.println(in.nextLine());
+    }
+
+    
+}
